317aab3a38cdd863fa3995d47b42ffb22910757c
YoussefSweden <79831898+YoussefSweden@users.noreply.github.com>
2021-05-08 12:59:09 +0200

    Add files via upload

---
+++ j2.js
@@ -0,0 +1,17 @@
+var ranomdized = Math.floor(Math.random() * 11);  
+function calculation()
+{
+    var guessedNumber = parseInt(document.getElementById("input").value);
+    ranomdized;   
+   if(guessedNumber==ranomdized)
+    {
+        document.getElementById("textOutput").innerHTML = "Good Work";
+    }
+    else
+    {
+        alert(ranomdized); //test för att se.
+        document.getElementById("textOutput").innerHTML = "Not matched";
+    }
+    
+}
+
